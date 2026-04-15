75fa51df2e892912e3edaf8968dfaa57fdf90fe2
An Hoang Nguyen <nguyenan.sadek@outlook.com>
2026-04-15 19:50:23 +0700

    Xong phan kiem tra tinh nhat quan node, gio qua segment roi toi street

--- utils.py
+++ utils.py
@@ -1,7 +1,12 @@
 from pathlib import Path
 import pandas as pd
+from matplotlib_venn import venn3
+import matplotlib.pyplot as plt
+
+from collections.abc import Iterable
 
 PATH = Path("data/raw")
+
 
 def load_raw_data(raw_path: Path=PATH) -> dict[str, pd.DataFrame]:
     nodes_df    = pd.read_csv(raw_path / "nodes.csv")
@@ -17,3 +22,35 @@ def load_raw_data(raw_path: Path=PATH) -> dict[str, pd.DataFrame]:
         "status": status_df,
         "train": train_df
     }
+
+def create_venn_diagram(A: set[int], B: set[int], C: set[int], figsize=(6, 6), label=""):
+    plt.figure(figsize=figsize)
+
+    # Tập riêng
+    _A = A - B - C
+    _B = B - A - C
+    _C = C - A - B
+
+    # Giao 2 tập (loại bỏ giao 3)
+    A_B = (A & B) - C
+    A_C = (A & C) - B
+    B_C = (B & C) - A
+
+    # Giao 3 tập
+    A_B_C = A & B & C
+
+    venn = venn3(
+        subsets=(
+            len(_A), len(_B), len(_C),
+            len(A_B), len(A_C), len(B_C),
+            len(A_B_C)
+        )
+    )
+
+    venn.get
+
+    plt.title(label)
+    plt.show()
+
+
+
