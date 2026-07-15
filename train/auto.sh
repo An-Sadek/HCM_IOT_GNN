3d474078dfbbca745ffbb018459ddb6d0e0465da
An Hoang Nguyen <nguyenan.sadek@outlook.com>
2026-07-15 22:44:37 +0700

    Doi script vi transformer moi nhat bi loi

--- train/auto.sh
+++ train/auto.sh
@@ -1,6 +1,20 @@
 #torchrun --standalone --nproc_per_node=gpu train/run_train.py
-conda create -y --name dgl python=3.12
+conda create -y --name dgl python=3.10
+
 conda activate dgl
+
+conda install -y pytorch==2.4.0 torchvision==0.19.0 torchaudio==2.4.0 pytorch-cuda=12.4 -c pytorch -c nvidia
+
 conda install -y -c dglteam/label/th24_cu124 dgl
-conda install pytorch==2.4.1 torchvision==0.19.1 torchaudio==2.4.1 pytorch-cuda=12.4 -c pytorch -c nvidia
-conda install -c dglteam/label/th24_cu124 dgl
+
+conda install -y pandas pyyaml scikit-learn pydantic
+
+python -m pip install "transformers==4.45.2" "tokenizers<0.21" accelerate safetensors sentencepiece protobuf
+
+cd ~
+
+git clone https://github.com/An-Sadek/HCM_IOT_GNN.git
+cd HCM_IOT_GNN
+python preprocess/pipelines/run_pipelines.
+python train/run_train.py
+
