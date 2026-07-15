c6b455adccff576792b59a3cd9260ab364d64ebc
An Hoang Nguyen <nguyenan.sadek@outlook.com>
2026-07-15 20:39:11 +0700

    Them test

--- train/auto.sh
+++ train/auto.sh
@@ -1,5 +1,6 @@
-torchrun --standalone --nproc_per_node=gpu train/run_train.py
-conda create --name dgl python=3.12
+#torchrun --standalone --nproc_per_node=gpu train/run_train.py
+conda create -y --name dgl python=3.12
 conda activate dgl
+conda install -y -c dglteam/label/th24_cu124 dgl
+conda install pytorch==2.4.1 torchvision==0.19.1 torchaudio==2.4.1 pytorch-cuda=12.4 -c pytorch -c nvidia
 conda install -c dglteam/label/th24_cu124 dgl
-pip install torch==2.6.0 torchvision==0.21.0 torchaudio==2.6.0 --index-url https://download.pytorch.org/whl/cu124 pyyaml scikit-learn transformers
