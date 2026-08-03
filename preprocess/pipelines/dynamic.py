979d1c4822785d35888ba2667df9223482089568
An Hoang Nguyen <nguyenan.sadek@outlook.com>
2026-08-03 14:53:28 +0700

    FFill vi du lieu qua thua

--- preprocess/pipelines/dynamic.py
+++ preprocess/pipelines/dynamic.py
@@ -60,6 +60,7 @@ class DynamicPreprocess(Preprocess):
             values="velocity"
         ).reindex(self.full_time)
 
+        velocity_arr = velocity_arr.ffill()
         velocity_observed_mask = velocity_mat.notna().astype(np.float32)
         velocity_arr = velocity_mat.clip(lower=1., upper=120.).fillna(0.0)
         
