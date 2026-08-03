d7ca58da95585e52e6a785a1e30d253d7dc0d83e
An Hoang Nguyen <nguyenan.sadek@outlook.com>
2026-08-03 15:17:24 +0700

    Xoa ffill

--- preprocess/pipelines/dynamic.py
+++ preprocess/pipelines/dynamic.py
@@ -60,7 +60,7 @@ class DynamicPreprocess(Preprocess):
             values="velocity"
         ).reindex(self.full_time)
 
-        velocity_observed_mask = velocity_arr.notna().astype(np.float32)
+        velocity_observed_mask = velocity_mat.notna().astype(np.float32)
         velocity_arr = velocity_mat.clip(lower=1., upper=120.).fillna(0.0)
         
         print("Kích thước của pivot table velocity trong status df:", velocity_arr.shape)
