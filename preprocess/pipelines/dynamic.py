e51f5a9296d04d057ef3d79fa531254cd7360ca7
An Hoang Nguyen <nguyenan.sadek@outlook.com>
2026-08-05 09:40:05 +0700

    Mask truoc roi moi fill

--- preprocess/pipelines/dynamic.py
+++ preprocess/pipelines/dynamic.py
@@ -60,8 +60,8 @@ class DynamicPreprocess(Preprocess):
             values="velocity"
         ).reindex(self.full_time)
 
+        velocity_observed_mask = velocity_mat.notna().astype(np.float32)
         velocity_arr = velocity_mat.ffill()
-        velocity_observed_mask = velocity_arr.notna().astype(np.float32)
         velocity_arr = velocity_arr.clip(lower=1., upper=120.).fillna(0.0)
         
         print("Kích thước của pivot table velocity trong status df:", velocity_arr.shape)
