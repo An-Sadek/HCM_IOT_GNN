ca71045e6a36cd500192e6b29906a71f55f4552a
An Hoang Nguyen <nguyenan.sadek@outlook.com>
2026-08-22 13:40:47 +0700

    Bo cac model bi loi

--- test/run.sh
+++
@@ -1,7 +0,0 @@
-python test/forecast_all.py --checkpoint result/_htgnn1/htgnn_best.pt --output result/_htgnn1/forecast_all.csv --device cuda --batch-size 16
-python test/forecast_all.py --checkpoint result/_htgnn2/htgnn_best.pt --output result/_htgnn2/forecast_all.csv --device cuda --batch-size 16
-python test/forecast_all.py --checkpoint result/_htgnn3/htgnn_best.pt --output result/_htgnn3/forecast_all.csv --device cuda --batch-size 16
-python test/forecast_all.py --checkpoint result/_htgnn4/htgnn_best.pt --output result/_htgnn4/forecast_all.csv --device cuda --batch-size 16
-
-python test/forecast_all.py --checkpoint result/_sehtgnn1/sehtgnn_best.pt --output result/_sehtgnn1/forecast_all.csv --device cuda --batch-size 16
-python test/forecast_all.py --checkpoint result/_sehtgnn2/sehtgnn_best.pt --output result/_sehtgnn2/forecast_all.csv --device cuda --batch-size 16
